1291f179866ab14e563951c54c8aae01033a30e1
Tsung Min Liu <miles151212@gmail.com>
2025-04-06 00:52:43 +0800

    add operator validation

--- calculator.py
+++ calculator.py
@@ -14,27 +14,34 @@ def divide(a, b):
 
 def main():
     print("Welcome to the Simple Calculator!")
+    while(True):
+        try:
+            a = float(input("The first number: "))
+            while(True):
+                operator = input("Operator (+, - ,* ,/ ): ")
+                if operator in ["+", "-", "*", "/"]:
+                    break
+                print("Please enter the correct operator!")
+            b = float(input("The second number: "))
 
-    try:
-        a = float(input("The first number: "))
-        operator = input("Operator (+, - ,* ,/ ): ")
-        b = float(input("The second number: "))
-
-        if operator == "+":
-            print("result: ", add(a, b))
-        elif operator == "-":
-            print("result: ", subtract(a, b))
-        elif operator == "*":
-            print("result: ", multiply(a, b))
-        elif operator == "/":
-            try:
-                print("result: ", divide(a, b))
-            except ValueError as e:
-                print(e)
-        else:
-            print("invalid operator")
-    except ValueError as e:
-        print("Invalid input. Please enter valid numbers.")
-        
+            if operator == "+":
+                print("result: ", add(a, b))
+            elif operator == "-":
+                print("result: ", subtract(a, b))
+            elif operator == "*":
+                print("result: ", multiply(a, b))
+            elif operator == "/":
+                try:
+                    print("result: ", divide(a, b))
+                except ValueError as e:
+                    print(e)
+            else:
+                print("invalid operator")
+        except ValueError as e:
+            print("Invalid input. Please enter valid numbers.")
+        again = input("continue? (y/n)")
+        if again.lower() != "y":
+            print("Thanks!")
+            break
 if __name__ == "__main__":
     main()
